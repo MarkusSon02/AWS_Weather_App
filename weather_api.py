a707179e03f487094f55e85676a3c318cc69caca
Son Tran <sonthanhtran2004@gmail.com>
2025-05-30 20:33:23 +0700

    Update weather_api.py with a for loop for multiple calls across a range of dates

--- weather_api.py
+++ weather_api.py
@@ -4,36 +4,38 @@ import functions
 url = "https://api.weatherstack.com/historical?access_key=d19781abe3457032c492f516530ec81d"
 
 start = '2025-01-01'
-end = '2025-04-30'
+end = '2025-05-29'
 
 date_list = functions.generate_date_list(start, end)
+print(len(date_list))
 print(date_list)
 print(date_list[0])
+print(date_list[-1])
 
-for date in date_list:
-    print(date)
-    querystring = {"query":"Edmonton", "historical_date":date}
+# for date in date_list:
+#     print(date)
+#     querystring = {"query":"Edmonton", "historical_date":date}
 
-    response = requests.get(url, params=querystring)
+#     response = requests.get(url, params=querystring)
 
-    data = response.json()
+#     data = response.json()
 
-    print(data)
+#     print(data)
 
-    par_locations = functions.partition_data(data)
-    print(par_locations)
+#     par_locations = functions.partition_data(data)
+#     print(par_locations)
 
-    par_dates = functions.partition_date(date)
-    print(par_dates)
+#     par_dates = functions.partition_date(date)
+#     print(par_dates)
 
-    params = par_locations + par_dates
+#     params = par_locations + par_dates
 
-    bucket = "weather-api-data-son"
+#     bucket = "weather-api-data-son"
 
-    if functions.is_dict(data):
-        functions.upload_partitioned_weather(data, params, bucket)
-    else:
-        print("Data is not in json format")
+#     if functions.is_dict(data):
+#         functions.upload_partitioned_weather(data, params, bucket)
+#     else:
+#         print("Data is not in json format")
 
 
 
